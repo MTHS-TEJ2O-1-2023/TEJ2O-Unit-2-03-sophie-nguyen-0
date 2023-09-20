31dbbba41fb6ac6642df4e4d5355d6596e55d0b2
sophie-nguyen-0 <113055711+sophie-nguyen-0@users.noreply.github.com>
2023-09-20 17:13:14 +0000

    python

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -1,10 +1,14 @@
 """
-Created by: Mr. Coxall
-Created on: Sep 2020
-This module is a Micro:bit MicroPython program
+Created by: Sophie
+Created on: Sep 2023
+This module is a Micro:bit MicroPython program that does math
 """
 
 from microbit import *
 
+display.clear()
+sleep(1)
 
-display.scroll("Hello, World!")
+display.scroll("a rectangle has dimensions of 5cm and 3cm.")
+display.scroll("the perimeter of a rectangle is " + str(2 * (5 + 3)) + "cm")
+display.scroll("the area of a rectangle is " + str(5 * 3) + "cm^2")
